6977a4988bffb9916d564433a3a6f6517ab597a9
JaviZamudio <javier.elihu100@gmail.com>
2024-04-19 17:03:10 -0600

    Layout Finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,84 @@
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem, Button } from "@nextui-org/react";
-export default function Header({ titulo }: { titulo: string }) {
-    return (
-        <header className="w-full h-20 flex justify-between items-center">
-            <div className="p-5 text-3xl font-bold">
-                <h2>{titulo}</h2>
-            </div>
-            <div className="flex m-5 text-xl">
-                <div className=" p-3">
-                    <Dropdown>
-                        <DropdownTrigger>
-                            <Button
-                                variant="bordered"
-                            >
-                                <span className="material-symbols-outlined">
-                                    notifications
-                                </span>
-                            </Button>
-                        </DropdownTrigger>
-                        <DropdownMenu aria-label="Static Actions">
-                            <DropdownItem key="new">New file</DropdownItem>
-                            <DropdownItem key="copy">Copy link</DropdownItem>
-                            <DropdownItem key="edit">Edit file</DropdownItem>
-                            <DropdownItem key="delete" className="text-danger" color="danger">
-                                Delete file
-                            </DropdownItem>
-                        </DropdownMenu>
-                    </Dropdown>
+"use client"
 
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem, Button, Popover, PopoverTrigger, PopoverContent, Divider, Badge } from "@nextui-org/react";
+export default function Header({ titulo }: { titulo: string }) {
+  return (
+    <header className="w-full py-4 px-2 flex justify-between items-center">
+      <h2 className="text-3xl font-bold">{titulo}</h2>
+
+      {/* Buttons */}
+      <div className="flex gap-4">
+        <Popover showArrow placement="bottom-end">
+          <PopoverTrigger>
+            <Button variant="flat" isIconOnly>
+              <span className="material-symbols-outlined">
+                notifications
+              </span>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="p-4 flex flex-col items-start">
+            <h1 className="text-lg font-medium">
+              Notifications
+            </h1>
+
+            <div className="flex flex-col gap-2 mt-2 w-64 h-56 overflow-hidden p-1 overflow-y-auto">
+              {/* <p className="text-sm opacity-75 p-4">
+                  You have no notifications
+                </p> */}
+
+              <Badge content="" color="primary">
+                <div className="flex flex-col gap-1 rounded-lg bg-content2 p-2 w-full">
+                  <p className="text-sm font-medium">
+                    Nueva solicitud
+                  </p>
+                  <p className="text-xs opacity-75">
+                    Tienes una nueva solicitud de cotización de parte del proveedor: <strong>Proveedor 1</strong>
+                  </p>
                 </div>
-                <div className="p-3">
-                    <span className="material-symbols-outlined">
-                        person
-                    </span>
+              </Badge>
+              <Badge content="" color="primary">
+                <div className="flex flex-col gap-1 rounded-lg bg-content2 p-2 w-full">
+                  <p className="text-sm font-medium">
+                    Nueva solicitud
+                  </p>
+                  <p className="text-xs opacity-75">
+                    Tienes una nueva solicitud de cotización de parte del proveedor: <strong>Proveedor 1</strong>
+                  </p>
                 </div>
+              </Badge>
+
+              <Divider />
+
+              <Badge content="" color="primary" isInvisible={true}>
+                <div className="flex flex-col gap-1 rounded-lg bg-content1 p-2 w-full">
+                  <p className="text-sm font-medium">
+                    Nueva solicitud
+                  </p>
+                  <p className="text-xs opacity-75">
+                    Tienes una nueva solicitud de cotización de parte del proveedor: <strong>Proveedor 1</strong>
+                  </p>
+                </div>
+              </Badge>
+              <Badge content="" color="primary" isInvisible={true}>
+                <div className="flex flex-col gap-1 rounded-lg bg-content1 p-2 w-full">
+                  <p className="text-sm font-medium">
+                    Nueva solicitud
+                  </p>
+                  <p className="text-xs opacity-75">
+                    Tienes una nueva solicitud de cotización de parte del proveedor: <strong>Proveedor 1</strong>
+                  </p>
+                </div>
+              </Badge>
             </div>
-        </header>
-    )
+          </PopoverContent>
+        </Popover>
+
+        <Button variant="faded" isIconOnly>
+          <span className="material-symbols-outlined">
+            person
+          </span>
+        </Button>
+      </div>
+    </header>
+  )
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,34 +3,76 @@
 import React from 'react';
 import NextLink from 'next/link';
 import { usePathname } from 'next/navigation';
+import { sidebar } from '@/constants';
+import { Listbox, ListboxSection, ListboxItem } from "@nextui-org/react"
 import Image from 'next/image';
-import { sidebarLinks } from '@/constants';
 
 export default function Sidebar() {
   const pathname = usePathname();
 
   return (
-    <section className='flex flex-grow flex-col max-w-60 justify-between bg-primary text-primary-foreground p-6 pt-28'>
-      <div className='flex flex-1 flex-col gap-6'>
+    <>
+      <section className='flex flex-grow flex-col max-w-60 justify-between bg-primary text-primary-foreground p-4'>
+        <div className='flex flex-col items-center justify-center my-4'>
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={80}
+            height={80}
+            className='mb-2'
+          />
+          <h2 className='text-center font-bold text-2xl tracking-widest'>Supplynk</h2>
+        </div>
+        <div className='flex flex-1 flex-col gap-6'>
+          <Listbox variant="flat" aria-label="Listbox menu with sections">
+            {sidebar.map((section, index) => (
+              <ListboxSection key={index} title={section.title} showDivider classNames={{
+                heading: 'text-primary-foreground opacity-75',
+                divider: 'bg-primary-foreground opacity-25',
+              }}>
+                {section.items.map((item, itemIndex) => (
+                  <ListboxItem key={itemIndex} className='hover:!bg-white px-4 py-2' startContent={<span className="material-symbols-outlined !text-xl">
+                    {item.icon}
+                  </span>}>
+                    {item.label}
+                  </ListboxItem>
+                ))}
+              </ListboxSection>
+            ))}
+          </Listbox>
+
+
+
+
+          {/*
+
         {sidebarLinks.map((item) => {
           const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
           return (
+
             <NextLink
               href={item.route}
               key={item.label}
-              className={`flex gap-4 items-center p-4 rounded-lg justify-start ${isActive ? 'bg-primary' : ''}`}
+              className={`flex gap-4 items-center p-4 rounded-lg justify-start`}
             >
               <span className="material-symbols-outlined">
                 {item.icon}
               </span>
-              <p className="text-lg font-semibold max-lg:hidden">
-                {item.label}
-              </p>
+              <Listbox variant="flat" aria-label="Listbox menu with sections">
+                <ListboxSection title="Actions" showDivider>
+                  <ListboxItem
+                    key="new"
+                    description="Create a new file">
+                    New File
+                  </ListboxItem>
+                </ListboxSection>
+              </Listbox>
             </NextLink>
           );
 
-        })}
-      </div>
-    </section>
+        })}/*/}
+        </div>
+      </section>
+    </>
   );
 }
